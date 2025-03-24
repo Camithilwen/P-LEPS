be58d00e383e7eaff0a0b58f5375eb61fe4edc3d
wgneighbors <wgneighbors22@gmail.com>
2025-03-24 14:51:24 -0400

    Added bones of prediciton architecture for reference.

---
+++ prediction/prediction.py
@@ -0,0 +1,19 @@
+from tensorflow.keras.models import load_model
+import pandas as pd
+import preprocessing as pp
+
+model = load_model("model/lem_dnn1.1.h5")
+
+# preprocess...
+# pp.check_eligible(gui_data.csv)
+# pp.prepprocess(gui_data.csv)
+
+new_data = pd.DataFrame({}) # replace with actual data from preprocessing.py
+predictions = model.predict(new_data)
+predictions_binary = (predictions > 0.5).astype(int)
+
+new_data["Predicted_Loan_Status"] = predictions_binary
+
+new_data.to_csv("predictions.csv", index=False)
+
+print("Predictions saved to predictions.csv")
