af2ab93d290575538a0c2b707178577fa0f864ad
Jamie Kemman <jamieruth@proton.me>
2025-04-13 00:47:33 -0400

    Updated build.yml and pyproject.toml

--- flake.nix
+++ flake.nix
@@ -1,31 +1,53 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    devenv = {
-      url = "github:cachix/devenv";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    devenv.url = "github:cachix/devenv";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    devenv,
-    ...
-  } @ inputs: {
-    devShells = (
-      nixpkgs.lib.genAttrs ["x86_64-linux"]
-      (system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            (python3.withPackages (py:
-                with py; [matplotlib pandas tkinter customtkinter scikit-learn tensorflow keras nuitka wheel setuptools cx-freeze]))
+  outputs = { self, nixpkgs, devenv, ... } @ inputs:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system} = {
+      loan-app = pkgs.stdenv.mkDerivation {
+        name = "loan-app";
+        src = ./.;
+        buildInputs = [
+          (pkgs.python3.withPackages (py: [
+            py.matplotlib py.pandas py.tkinter py.customtkinter
+            py.scikit-learn py.tensorflow py.keras py.nuitka
+          ]))
+          pkgs.libxcrypt
+        ];
+        installPhase = ''
+          mkdir -p $out/bin
+          cp gui_5_0 $out/bin/
+        '';
+      };
+
+      dockerImage = pkgs.dockerTools.buildImage {
+        name = "loan-app";
+        tag = "latest";
+        contents = [ self.packages.${system}.loan-app pkgs.bashInteractive ];
+        config = {
+          Cmd = [ "${self.packages.${system}.loan-app}/bin/gui_5_0" ];
+          Env = [
+            "PYTHONPATH=/app"
+            "LD_LIBRARY_PATH=${pkgs.libxcrypt}/lib"
           ];
         };
+      };
+    };
 
-      })
-    );
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        (python3.withPackages (py: [
+          py.matplotlib py.pandas py.tkinter py.customtkinter
+          py.scikit-learn py.tensorflow py.keras py.nuitka
+        ]))
+        xorg.xhost
+      ];
+    };
   };
 }
